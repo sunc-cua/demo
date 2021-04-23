99de611085291a82758e5c39bddcab0335994550
sunc-cua <sunc@cua.edu>
2021-04-23 12:47:47 -0400

    restarted

---
+++ Demo1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+
+st.write("This is the first write!")
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
